11ccca7138c2c44984a7ad7dc68a7ba6c42d5b02
Niklas Treml <treml.niklas@gmail.com>
2023-05-28 13:01:36 +0200

    mini.animate setup

--- dot_config/nvim/lua/user/plugins/user.lua
+++ dot_config/nvim/lua/user/plugins/user.lua
@@ -1,13 +1,19 @@
 return {
-    {
-      "kylechui/nvim-surround",
-      version = "*", -- Use for stability; omit to use `main` branch for the latest features
-      event = "VeryLazy",
-      config = function()
-          require("nvim-surround").setup({
-              -- Configuration here, or leave empty to use defaults
-          })
-      end
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
   },
-  { 'echasnovski/mini.animate', version = false },
+  {
+    'echasnovski/mini.animate',
+    version = false,
+    config = function()
+      require('mini.animate').setup()
+    end
+  },
 }
