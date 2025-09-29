413cc71dc04fba2239e5dff296038719f8c9f6f6
Niklas Treml <treml.niklas@gmail.com>
2025-09-29 11:19:50 +0200

    remove unnecessary bindings
    Signed-off-by: Niklas Treml <treml.niklas@gmail.com>

--- dot_config/nvim/lua/ntreml/plugins/copilot.lua
+++ dot_config/nvim/lua/ntreml/plugins/copilot.lua
@@ -6,12 +6,8 @@ return {
                 suggestion = {
                     auto_trigger = true,
                     keymap = {
-                        jump_prev = "[[",
-                        jump_next = "]]",
                         accept = "<C-j>",
                         dismiss = "<M-j>",
-                        refresh = "gr",
-                        open = "<M-CR>"
                     },
                 }
             }
