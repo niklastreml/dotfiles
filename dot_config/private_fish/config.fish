31657535ae496ca10fb61a10c8c31bbc3776edbf
Niklas Treml <treml.niklas@gmail.com>
2025-10-23 11:22:37 +0200

    env support
    Signed-off-by: Niklas Treml <treml.niklas@gmail.com>

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+source $HOME/.env.fish
 starship init fish | source
 set -g -x CMAKE_EXPORT_COMPILE_COMMANDS ON
 if type -q ninja
@@ -9,7 +10,14 @@ if type -q ninja
 end
 set -g -x CMAKE_BUILD_PARALLEL_LEVEL $(nproc)
 
-set -g -x EDITOR nvim
-set -g -x TERMINAL ghostty
 bind tab complete-and-search
 bind ctrl-j forward-char
+
+set -g -x EDITOR nvim
+set -g -x TERMINAL ghostty
+
+fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/.local/bin
+fish_add_path $HOME/.local/go/bin
+fish_add_path $HOME/.local/nvim/bin
+fish_add_path $HOME/.local/krew/bin
