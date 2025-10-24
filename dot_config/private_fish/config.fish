208ff1c95c17ea65741f26edec9d7236ea925191
Niklas Treml <treml.niklas@gmail.com>
2025-10-25 01:55:42 +0200

    set cmake env variables
    Signed-off-by: Niklas Treml <treml.niklas@gmail.com>

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,8 +3,13 @@ if status is-interactive
 end
 
 starship init fish | source
+set -g -x CMAKE_EXPORT_COMPILE_COMMANDS ON
+if type -q ninja
+    set -g -x CMAKE_GENERATOR Ninja
+end
+set -g -x CMAKE_BUILD_PARALLEL_LEVEL $(nproc)
 
-set -g -x EDITOR "nvim"
-set -g -x TERMINAL "ghostty"
+set -g -x EDITOR nvim
+set -g -x TERMINAL ghostty
 bind tab complete-and-search
 bind ctrl-j forward-char
